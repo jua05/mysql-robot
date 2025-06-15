f7ded45665a72a12cb8d95e730e473a9053fc0e1
jua05 <sjua1306@gmail.com>
2025-06-15 20:25:59 +0900

    ✨ CRUD 추가

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20240652/mysql_robot/RobotUpdateDto.kt
@@ -0,0 +1,8 @@
+package kr.ac.kumoh.ce.s20240652.mysql_robot
+
+data class RobotUpdateDto(
+    val name: String?,
+    val model: String?,
+    val manufacturer: String?,
+    val robotClass: String?
+)
